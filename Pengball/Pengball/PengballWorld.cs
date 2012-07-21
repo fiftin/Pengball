d6ad59a5be8b6d02b573bf54299f828de8cb6f70
Denis Gukov <denguk@gmail.com>
2012-07-21 22:57:26 +0600

    1) Добавил в класс PengballWorld свойство Size - размер игрового простанства. 2) Заменил некоторые свойства в классе PengballWorld на упрощенный вид {get;set;}

--- Pengball/Pengball/PengballWorld.cs
+++ Pengball/Pengball/PengballWorld.cs
@@ -25,7 +25,8 @@ namespace Pengball
         public PengballWorld(GraphicsDevice graphics, ContentManager content, bool gameplay = true, string screenplay = null, bool visible = true)
             : base(graphics, content, visible)
         {
-            Viewport = new PengViewport(0, 0, 5, 3);
+            Size = new Vector2(5, 3);
+            Viewport = new PengViewport(Size);
             Screenplay = screenplay;
             if (Screenplay == null)
                 Screenplay = Content.Load<string>("screenplays/screenplay1");
@@ -55,31 +56,33 @@ namespace Pengball
         }
 
         private bool gameplay;
-        GameStopReason gameStopReason = GameStopReason.None;
-        private bool gameStopped = false;
+        
         private DateTime? stopTime;
         private DateTime? startTime;
 
-        public bool GameStopped { get { return gameStopped; } }
+        public Vector2 Size { get; set; }
+        public GameStopReason GameStopReason { get; private set; }
+        public bool GameStopped { get; private set; }
         public string Screenplay { get; private set; }
         public Ball Ball { get; private set; }
         public Player LeftPlayer { get; private set; }
         public Player RightPlayer { get; private set; }
         public Tree Tree { get; private set; }
+
         public void StopGame(GameStopReason reason = GameStopReason.None)
         {
             UnactivePlayers();
-            gameStopped = true;
+            GameStopped = true;
             stopTime = DateTime.Now;
             startTime = null;
-            gameStopReason = reason;
+            GameStopReason = reason;
         }
 
         public void StartGame()
         {
-            if (gameStopReason == GameStopReason.GoalToLeft)
+            if (GameStopReason == GameStopReason.GoalToLeft)
                 ((Ball)Objects["ball"]).ResetToRight();
-            else if (gameStopReason == GameStopReason.GoalToRight)
+            else if (GameStopReason == GameStopReason.GoalToRight)
                 ((Ball)Objects["ball"]).ResetToLeft();
             else
                 ((Ball)Objects["ball"]).ResetToLeft();
@@ -88,9 +91,9 @@ namespace Pengball
             Objects["leftPlayer"].Body.RestoreCollisionWith(Objects["ball"].Body);
             Objects["rightPlayer"].Body.RestoreCollisionWith(Objects["ball"].Body);
             stopTime = null;
-            gameStopped = false;
+            GameStopped = false;
             startTime = DateTime.Now;
-            gameStopReason = GameStopReason.None;
+            GameStopReason = GameStopReason.None;
         }
 
         void UnactivePlayers()
@@ -101,7 +104,7 @@ namespace Pengball
 
         void ball_Contacted(object sender, PengContactEventArgs e)
         {
-            if (!gameStopped && gameplay)
+            if (!GameStopped && gameplay)
             {
                 var ball = (Ball)sender;
                 if (e.Contactee.Name == "footer")
@@ -125,7 +128,7 @@ namespace Pengball
             if (gameplay)
             {
                 var kbState = Keyboard.GetState();
-                if (gameStopReason == GameStopReason.GoalToLeft || gameStopReason == GameStopReason.GoalToRight)
+                if (GameStopReason == GameStopReason.GoalToLeft || GameStopReason == GameStopReason.GoalToRight)
                 {
                     if (DateTime.Now - stopTime >= TimeSpan.FromSeconds(2))
                     {
